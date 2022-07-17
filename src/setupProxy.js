b8e4483e4d8026cd2609227a10b6cff45204cc5b
DavidLee1216 <55806398+DavidLee1216@users.noreply.github.com>
2022-07-17 17:41:26 +0800

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://127.0.0.1:8000",
+      target: process.env.REACT_APP_SERVER_URL,
       changeOrigin: true,
     })
   );
